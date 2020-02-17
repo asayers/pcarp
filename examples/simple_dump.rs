1b6196ca469c386133b190f942b10c1892c25c67
Alex Sayers <alex@asayers.com>
2020-02-17 09:12:44 +0900

    Add "simple_dump" binary, for benchmarking

---
+++ examples/simple_dump.rs
@@ -0,0 +1,44 @@
+use pcap::*;
+use std::fs::File;
+use std::time::*;
+
+fn main() {
+    let mut args = std::env::args();
+    let _ = args.next();
+    let backend = args.next().unwrap();
+    let path = args.next().unwrap();
+    match backend.as_str() {
+        "pcarp" => {
+            let file = File::open(&path).unwrap();
+            let mut pcap = pcarp::Capture::new(file).unwrap();
+            while let Some(pkt) = pcap.next() {
+                match pkt {
+                    Ok(pkt) => {
+                        let ts = pkt.timestamp.unwrap_or(SystemTime::UNIX_EPOCH);
+                        println!("{:?}", ts);
+                    }
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }
+        "libpcap" => {
+            let mut pcap = Capture::from_file(path).unwrap();
+            loop {
+                match pcap.next() {
+                    Ok(pkt) => {
+                        let ts = SystemTime::UNIX_EPOCH
+                            + Duration::new(
+                                pkt.header.ts.tv_sec as u64,
+                                pkt.header.ts.tv_usec as u32 * 1000,
+                            );
+                        println!("{:?}", ts);
+                    }
+                    Err(Error::NoMorePackets) => break,
+                    Err(Error::PcapError(_)) => (),
+                    Err(e) => panic!("{}", e),
+                }
+            }
+        }
+        x => panic!("Unknown: {}", x),
+    }
+}
