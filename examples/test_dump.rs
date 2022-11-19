4136935cc85921582f6162e9b7e8dbd4518a7aec
Alex Sayers <alex@asayers.com>
2022-11-19 16:40:33 +0900

    integration: Test the new data_offset field

--- examples/test_dump.rs
+++ examples/test_dump.rs
@@ -1,11 +1,14 @@
 use pcarp::*;
 use sha1::{Digest, Sha1};
+use std::os::unix::fs::FileExt;
 
 fn main() {
     env_logger::init();
     let path = std::path::PathBuf::from(std::env::args().nth(1).unwrap());
     let file = std::fs::File::open(&path).unwrap();
     let mut pcap = Capture::new(file).unwrap();
+    let file = std::fs::File::open(&path).unwrap();
+    let mut buf = vec![0; 1024 * 1024];
     while let Some(pkt) = pcap.next() {
         let pkt = pkt.unwrap();
         let ts = pkt
@@ -13,6 +16,10 @@ fn main() {
             .unwrap()
             .duration_since(std::time::SystemTime::UNIX_EPOCH)
             .unwrap();
+        let n_bytes = pkt.data_offset.end - pkt.data_offset.start;
+        file.read_exact_at(&mut buf[..n_bytes], pkt.data_offset.start as u64)
+            .unwrap();
+        assert_eq!(pkt.data, &buf[..n_bytes]);
         println!(
             "{:0>10}.{:0>9} {:>6} {:x}",
             ts.as_secs(),
