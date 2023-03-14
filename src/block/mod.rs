999ee512b3e701fb137c208e4373f7cff0a8cc2e
Alex Sayers <alex@asayers.com>
2023-03-15 08:58:34 +0900

    Enumerate the block types without data

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -36,18 +36,57 @@ use crate::{Error, Result};
 use byteorder::ByteOrder;
 use tracing::*;
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum BlockType {
+    SectionHeader,
+    InterfaceDescription,
+    ObsoletePacket,
+    SimplePacket,
+    NameResolution,
+    InterfaceStatistics,
+    EnhancedPacket,
+    IRIGTimestamp,
+    Arinc429,
+    SystemdJournalExport,
+    DecryptionSecrets,
+    Custom,
+    Hone,
+    Sysdig,
+    Unknown(u32),
+}
+
+impl From<u32> for BlockType {
+    fn from(code: u32) -> Self {
+        match code {
+            0x0A0D_0D0A => BlockType::SectionHeader,
+            0x0000_0001 => BlockType::InterfaceDescription,
+            0x0000_0002 => BlockType::ObsoletePacket,
+            0x0000_0003 => BlockType::SimplePacket,
+            0x0000_0004 => BlockType::NameResolution,
+            0x0000_0005 => BlockType::InterfaceStatistics,
+            0x0000_0006 => BlockType::EnhancedPacket,
+            0x0000_0007 => BlockType::IRIGTimestamp,
+            0x0000_0008 => BlockType::Arinc429,
+            0x0000_0009 => BlockType::SystemdJournalExport,
+            0x0000_000A => BlockType::DecryptionSecrets,
+            0x0000_0101 | 0x40000102 => BlockType::Hone,
+            0x0000_0201..=0x0000_0213 => BlockType::Sysdig,
+            0x0000_0BAD | 0x40000BAD => BlockType::Custom,
+            n => BlockType::Unknown(n),
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Block<'a> {
-    SectionHeader(SectionHeader<'a>),               // 0x0A0D0D0A
-    InterfaceDescription(InterfaceDescription<'a>), // 0x00000001
-    ObsoletePacket(ObsoletePacket<'a>),             // 0x00000002
-    SimplePacket(SimplePacket),                     // 0x00000003
-    NameResolution(NameResolution),                 // 0x00000004
-    InterfaceStatistics(InterfaceStatistics<'a>),   // 0x00000005
-    EnhancedPacket(EnhancedPacket<'a>),             // 0x00000006
-    IRIGTimestamp,                                  // 0x00000007, ignored
-    Arinc429,                                       // 0x00000008, ignored
-    Unknown(u32),
+    SectionHeader(SectionHeader<'a>),
+    InterfaceDescription(InterfaceDescription<'a>),
+    ObsoletePacket(ObsoletePacket<'a>),
+    SimplePacket(SimplePacket),
+    NameResolution(NameResolution),
+    InterfaceStatistics(InterfaceStatistics<'a>),
+    EnhancedPacket(EnhancedPacket<'a>),
+    Unparsed(BlockType),
 }
 
 impl<'a> Block<'a> {
@@ -56,7 +95,7 @@ impl<'a> Block<'a> {
             // Looks like the pcap is truncated.  Let's just skip over the rest.
             return (buf.len(), Err(e));
         }
-        let block_type = B::read_u32(&buf[..4]);
+        let block_type = BlockType::from(B::read_u32(&buf[..4]));
         let block_length = B::read_u32(&buf[4..8]) as usize;
         let block = || {
             if block_length < 12 {
@@ -73,17 +112,16 @@ impl<'a> Block<'a> {
             if block_length != block_length_2 {
                 return Err(Error::BlockLengthMismatch);
             }
+            use BlockType as BT;
             let block = match block_type {
-                0x0A0D_0D0A => Block::from(SectionHeader::parse::<B>(body)?),
-                0x0000_0001 => Block::from(InterfaceDescription::parse::<B>(body)?),
-                0x0000_0002 => Block::from(ObsoletePacket::parse::<B>(body)?),
-                0x0000_0003 => Block::from(SimplePacket::parse::<B>(body)?),
-                0x0000_0004 => Block::from(NameResolution::parse::<B>(body)?),
-                0x0000_0005 => Block::from(InterfaceStatistics::parse::<B>(body)?),
-                0x0000_0006 => Block::from(EnhancedPacket::parse::<B>(body)?),
-                0x0000_0007 => Block::IRIGTimestamp,
-                0x0000_0008 => Block::Arinc429,
-                n => Block::Unknown(n),
+                BT::SectionHeader => SectionHeader::parse(block_data, endianness)?.into(),
+                BT::InterfaceDescription => InterfaceDescription::parse(block_data, endianness)?.into(),
+                BT::ObsoletePacket => ObsoletePacket::parse(block_data, endianness)?.into(),
+                BT::SimplePacket => SimplePacket::parse(block_data, endianness)?.into(),
+                BT::NameResolution => NameResolution::parse(block_data, endianness)?.into(),
+                BT::InterfaceStatistics => InterfaceStatistics::parse(block_data, endianness)?.into(),
+                BT::EnhancedPacket => EnhancedPacket::parse(block_data, endianness)?.into(),
+                _ => Block::Unparsed(block_type),
             };
             Ok(block)
         };
